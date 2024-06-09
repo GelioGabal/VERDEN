f30b459e70c578b92be7b3d614ebf87fefdc4acf
Stephan0404 <stephan906082@gmail.com>
2024-06-09 21:52:41 +0300

    spawn Unit

---
+++ Assets/scripts/spawnUnit.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnUnit : MonoBehaviour
+{
+    [NonSerialized]
+    public bool isEnemy = false;
+    public GameObject fly_1;
+    public float timeSpawn = 1f;
+    private void Update()
+    {
+        StartCoroutine(SpawnCar());
+    }
+    
+    IEnumerator SpawnCar()
+    {
+        
+
+            yield return new WaitForSeconds(timeSpawn);
+            Vector3 pos = new Vector3(
+                transform.GetChild(0).position.x + 10f,
+                transform.GetChild(0).position.y,
+                transform.GetChild(0).position.z -50f);
+            //var rnd = new Unity.Mathematics.Random();
+            //int randEnemy = rnd.NextInt(1,3);
+
+
+            GameObject spawn = Instantiate(fly_1, transform.GetChild(1).position, Quaternion.identity);
+
+
+            if (isEnemy)
+            {
+                spawn.tag = "Enemy";
+            }
+        
+    }
+}

6371e789f41d0b90fd5b1d1d1e9aa020e798c5e0
Stephan0404 <stephan906082@gmail.com>
2024-06-10 05:14:13 +0300

    Fix Spawn

--- Assets/scripts/spawnUnit.cs
+++ Assets/scripts/spawnUnit.cs
@@ -9,20 +9,21 @@ public class spawnUnit : MonoBehaviour
     public bool isEnemy = false;
     public GameObject fly_1;
     public float timeSpawn = 1f;
-    private void Update()
+    private void Start()
     {
         StartCoroutine(SpawnCar());
     }
     
     IEnumerator SpawnCar()
     {
-        
 
+        for (int i = 1; i <= 1000; i++)
+        {
             yield return new WaitForSeconds(timeSpawn);
             Vector3 pos = new Vector3(
                 transform.GetChild(0).position.x + 10f,
                 transform.GetChild(0).position.y,
-                transform.GetChild(0).position.z -50f);
+                transform.GetChild(0).position.z - 50f);
             //var rnd = new Unity.Mathematics.Random();
             //int randEnemy = rnd.NextInt(1,3);
 
@@ -34,6 +35,7 @@ public class spawnUnit : MonoBehaviour
             {
                 spawn.tag = "Enemy";
             }
+        }
         
     }
 }
